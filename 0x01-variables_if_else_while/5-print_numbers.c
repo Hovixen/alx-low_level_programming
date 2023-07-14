ee4650127a8259adc7e6c8e01962eccb91f862a5
Hovixen <orevaerijo@gmail.com>
2023-07-14 13:28:20 -0700

    taks 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * this code prints the single digits in base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10 && i != 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
