7d869a6c1c43e01836460eb850ecf68e0ce1416a
Hovixen <orevaerijo@gmail.com>
2023-07-14 11:11:27 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	int n;
 	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ";
 
-	for (n = 0; n <= 52; n++)
+	for (n = 0; n < 52; n++)
 	{
 		putchar(alph[n]);
 	}
