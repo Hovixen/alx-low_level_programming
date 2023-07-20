86c485c9229fd0539d1e043ea77df31b3c47bc2d
Hovixen <orevaerijo@gmail.com>
2023-07-20 11:09:57 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,16 +12,12 @@ void more_numbers(void)
 	char n[] = "01234567891011121314";
 	int i, j;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i < 10 ; i++)
 	{
-		j = 0;
-		while (j < 20)
+		for (j = 0 ; j < 20 ; j++)
 		{
 			_putchar(n[j]);
-			j++;
 		}
-		i++;
 		_putchar('\n');
 	}
 	_putchar('\n');
