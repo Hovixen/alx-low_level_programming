a5d0a5a908e9f4cc78bbf610c19859c446864de9
Hovixen <orevaerijo@gmail.com>
2023-07-20 06:41:18 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - This checks for uppercase characters
+ *
+ * @c: This is the defined integer for the function
+ *
+ * Return: returns 1 when c is uppercase and 0 when c is not uppercase
+ */
+int _isupper(int c)
+{
+	for (c = 'A'; c < 'Z' c++)
+	{
+		if (c <= 'Z')
+			return (1);
+		else
+			return (0);
+	}
+}
