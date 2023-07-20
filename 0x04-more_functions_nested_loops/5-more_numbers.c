27f111b242b9a87e355dfa9d3803114053dafe4a
Hovixen <orevaerijo@gmail.com>
2023-07-20 10:20:22 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - This function prints integers 0 - 14
+ * in 10 different rows
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char n[] = "01234567891011121314";
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar('\n');
+		j = 0;
+		while (j < 20)
+		{
+			_putchar(n[j]);
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -26,4 +26,5 @@ void print_alphabet_x10(void);
 
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 #endif
