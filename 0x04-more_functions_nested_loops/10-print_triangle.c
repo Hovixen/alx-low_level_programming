45e4fcc30acf04545d6fcde8372f4e793597b462
Hovixen <orevaerijo@gmail.com>
2023-07-21 04:53:31 -0700

    task 12

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,15 +7,29 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int n, j;
 
-	for (i = 0; i < size; i++)
+	for (n = 0; n < size; i++)
 	{
-		for (j = 1; j < (size - i); j++)
+ /**
+ * The first inner loop ends by giving j the value of j++
+ * i.e if j++ is 2 by the end of the control
+ * then the value of j would be 2 for the next control
+ * where i is still the value of the row
+ */
+		for (j = 1; j < (size - n); j++)
+		{
 			_putchar(' ');
+		}
+	/**
+	 * the second inner loop starts with the value of
+	 * j++ from the first loop control
+	 */
 		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
+		{
+			_putchar('#');
+		}
+		if (n < (size - 1))
 			_putchar('\n');
 	}
 	_putchar('\n');
