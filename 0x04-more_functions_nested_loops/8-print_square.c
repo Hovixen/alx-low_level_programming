f28be1b71380a598762b6d52fe42827cb0b340a1
Hovixen <orevaerijo@gmail.com>
2023-07-20 19:24:05 -0700

    task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - This function prints the squares of a given size
+ *
+ * @size: This is the size of the square (#)
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int j, k; /*declears the row (j) and column (k)*/
+
+	for (j = 0; j < size; j++)
+	{
+		for (k = 0; k < size; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
