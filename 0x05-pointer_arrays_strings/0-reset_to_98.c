00a359b3a7136e5908c59fddeb3638339dd6f2fa
Hovixen <orevaerijo@gmail.com>
2023-07-24 06:05:08 -0700

    task 0

---
+++ 0x05-pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - This function takes pointer to an int
+ * as parameter and updates it to 98.
+ *
+ * @n: This is the integer used.
+ * Retun: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointer_arrays_strings/main.h
@@ -0,0 +1,35 @@
+#ifndef main_h
+#define main_h
+
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int _putchar(char);
+int _islower(int c);
+int add(int a, int b);
+int _isalpha(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+void jack_bauer(void);
+void times_table(void);
+void pr_digit(int charcode, int fmt);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
+void pr_3_digit(int charcode, int fmt);
+void print_times_table(int n);
+void print_spaces(int n);
+void print_zero(void);
+void print_alphabet (void);
+void print_alphabet_x10(void);
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
