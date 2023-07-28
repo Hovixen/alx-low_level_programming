aa5b2eb48cb8e01b953c01b96cf2d46c82771979
Hovixen <orevaerijo@gmail.com>
2023-07-28 10:41:17 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 
 	while (str[length] != 0)
 	{
-		length++
+		length++;
 	}
 
-	for (i = 0; i <= length ; i + 2)
-		_putchar(str[length]);
+	for (i = 0; i <= length ; i = i + 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+
+void puts2(char *);
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
+	char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	str = "Holberton!\0Holberton";
+	puts2(str);
+	return (0);
 }
