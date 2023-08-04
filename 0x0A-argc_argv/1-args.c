c3429a5b97bba0c3dd7db2775f7c756ccc00a0bb
Hovixen <orevaerijo@gmail.com>
2023-08-04 13:03:45 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the number of argument passed to it
+ *
+ * @argc: number of comman line argument passed
+ * @argv: array of pointers of command line..
+ * Return: always 0 success
+ */
+
+int main (int argc , char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
