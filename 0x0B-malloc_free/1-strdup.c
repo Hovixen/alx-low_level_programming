f54230839e4005ec381cdabca121df5a93459379
Hovixen <orevaerijo@gmail.com>
2023-08-07 14:03:43 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - Function returns a copy of string given as parameter
+ *
+ * @str: string parameter argument
+ * Return: returns the pointer array to newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	char *sr;
+	int n = 0; /*initializes the count for the string*/
+	int i;
+
+	if (str == NULL)
+		return (0);
+	while (*(str + n) != '\0')
+	{
+		n++;
+	}
+	n++;
+	sr = (char *)malloc(sizeof (char) * n);
+	if (sr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < n ; i++)
+	{
+		sr[i] = str[i];
+	}
+	return (sr);
+}
