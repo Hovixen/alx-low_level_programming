c62b2538fc0b00e3ef4b23fdf857446416eba151
Hovixen <orevaerijo@gmail.com>
2023-08-15 01:36:40 +0000

    task 0

---
+++ dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure stores the information about a dog
+ * @name: assiged as the name of the dog
+ * @age: assigned as the age of the dog
+ * @owner: assigned the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
