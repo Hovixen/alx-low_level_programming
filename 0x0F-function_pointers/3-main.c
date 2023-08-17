1e1e91bd3d375dfefc8b7a7376c98af69ff48009
Hovixen <orevaerijo@gmail.com>
2023-08-17 10:14:29 +0000

    task 3 fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -21,7 +20,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	/*checks if it is only one operator sign that was used*/
-	if (strlen(argv[2]) != 1)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
