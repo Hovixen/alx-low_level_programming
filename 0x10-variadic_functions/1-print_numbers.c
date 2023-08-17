64891f9b25b29e44d9e1e1c2b7c9199f99ba8e78
Hovixen <orevaerijo@gmail.com>
2023-08-17 20:28:46 +0000

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function prints numbers follow by a new line
+ * @separator: pointer variable (string) to be printed between numbers
+ * @n: argument parameter passed
+ * Return: returns void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int value;
+	va_list num;/*declaring list for num*/
+	
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(num, int);/*get next argument of type int*/
+		printf("%d", value);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(num);
+	printf("\n");
+}
