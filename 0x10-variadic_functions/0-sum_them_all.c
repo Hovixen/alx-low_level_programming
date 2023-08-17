10222f9ce976fca971d458fd9eafeebe227c2fd1
Hovixen <orevaerijo@gmail.com>
2023-08-17 19:25:00 +0000

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, value, sum = 0;
-	va_list in_var;
+	unsigned int i;
+	int value, sum = 0;
+	va_list in_var; /* declaring the list*/
 
 	if (n == 0)
 		return (0);
-	va_start(in_var, n);
+	va_start(in_var, n); /*referencing the list*/
 
 	for (i = 0; i < n; i++)
 	{
-		value = va_arg(in_var, int);
+		value = va_arg(in_var, int);/*taking the argument for other parameters*/
 		sum += value;
 	}
-	va_end(in_var);
+	va_end(in_var);/*free the list*/
 	return (sum);
 }
