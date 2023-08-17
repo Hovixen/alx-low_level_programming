f640a6f380627e6a652f6742b224a64a734282eb
Hovixen <orevaerijo@gmail.com>
2023-08-17 10:28:45 +0000

    task 3 fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	sign = argv[2];
-	if (sign == NULL || ((*sign == '/' || *sign == '%') && b == 0))
+	if (sign == NULL)
 	{
 		printf("Error\n");
 		exit(98);
