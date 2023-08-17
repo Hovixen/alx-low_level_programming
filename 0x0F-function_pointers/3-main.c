9d797a3882c8e2d13186c2352caeaa2ca77bb13b
Hovixen <orevaerijo@gmail.com>
2023-08-17 15:29:23 +0000

    task 3 function pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,38 +10,39 @@
  */
 int main(int argc, char *argv[])
 {
-        int result = 0, a = 0, b = 0;
-        int (*f)(int, int);/*function pointer*/
-        char sign;
+	int result = 0, a = 0, b = 0;
+	int (*f)(int, int);/*function pointer*/
+	char *sign;
 
-        /*checks the number of arguments passed*/
-        if (argc != 4)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-        /*a = atoi(argv[1]);
-        b = atoi(argv[3]);*/
-        f = get_op_func(argv[2]);
+	/*checks the number of arguments passed*/
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/*a = atoi(argv[1]);
+	 * b = atoi(argv[3]);*/
+	f = get_op_func(argv[2]);
 
-        /*checks if it is only one operator sign that was used*/
-        if (f == NULL)
-        {
-                printf("Error\n");
-                exit(99);
-        }
+	sign = argv[2];
+	/*checks if it is only one operator sign that was used*/
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-        /*a = atoi(argv[1]);
-        b = atoi(argv[3]);*/
-        sign = *argv[2];
-        if (!sign) /*|| ((*sign == '/' || *sign == '%') && b == 0))*/
-        {
-                printf("Error\n");
-                exit(100);
-        }
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
-        result = f(a, b);
-        printf("%d\n", result);
-        return (0);
+	/*a = atoi(argv[1]);
+	 * b = atoi(argv[3]);*/
+	/*sign = *argv[2];*/
+	if (sign == NULL) /*|| ((*sign == '/' || *sign == '%') && b == 0))*/
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = f(a, b);
+	printf("%d\n", result);
+	return (0);
 }
