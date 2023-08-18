72dad17074b4565d0eacc4ad452fdd62551eb9c3
Hovixen <orevaerijo@gmail.com>
2023-08-18 01:23:17 +0000

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+/**
+ * print_all - Function that prints anything.
+ * @format: list of types of argument passed.
+ * Return: void.
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	va_list args;/*initializing the list argument*/
+	/*va_start(args, format);*/
+	char c;
+	int i_val;
+	float f_val;
+	char *s_val;
+	int separator_indicator = 0;
+
+	va_start(args, format);
+	while (format != 0 && format[i] != '\0')
+	{
+		c = format[i];
+		if (separator_indicator != 0)
+			printf(", ");
+
+		if (c == 'c')
+		{
+			i_val = va_arg(args, int); /*char being promoted to an int*/
+			printf("%c", i_val);
+		}
+		else if (c == 'i')
+		{
+			i_val = va_arg(args, int);
+			printf("%d", i_val);
+		}
+		else if (c == 'f')
+		{
+			f_val = va_arg(args, double);/*float being promoted to a double*/
+			printf("%f", f_val);
+		}
+		else if (c == 's')
+		{
+			s_val = va_arg(args, char *);
+			if (s_val == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s_val);
+		}
+		separator_indicator = 1;
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
