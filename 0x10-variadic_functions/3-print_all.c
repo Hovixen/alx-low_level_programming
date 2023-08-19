b87614088d456dca901e5f868706c47f7b895b61
Hovixen <orevaerijo@gmail.com>
2023-08-19 08:41:31 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,19 +3,17 @@
 #include <stddef.h>
 #include "variadic_functions.h"
 /**
- * print_all - Function that prints anything.
- * @format: list of types of argument passed.
- * Return: void.
- */
+ ** print_all - Function that prints anything.
+ ** @format: list of types of argument passed.
+ ** Return: void.
+ **/
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	va_list args;/*initializing the list argument*/
-	char c;
-	int i_val;/*, separator_indicator = 0;*/
-	float f_val;
-	char *s_val;
 	int separator_indicator = 0;
+	char c;
+	char *s_val;
 
 	va_start(args, format);
 	while (format && format[i])
@@ -26,16 +24,16 @@ void print_all(const char * const format, ...)
 		switch (c)
 		{
 			case 'c':
-				i_val = va_arg(args, int); /*char being promoted to an int*/
-				printf("%c", i_val);
+				/*i_val = va_arg(args, int); *char being promoted to an int*/
+				printf("%c", va_arg(args, int)), separator_indicator = 1;
 				break;
 			case 'i':
-				i_val = va_arg(args, int);
-				printf("%d", i_val);
+				/*i_val = va_arg(args, int);*/
+				printf("%d", va_arg(args, int)), separator_indicator = 1;
 				break;
 			case 'f':
-				f_val = va_arg(args, double);/*float being promoted to a double*/
-				printf("%f", f_val);
+				/*f_val = va_arg(args, double);*float being promoted to a double*/
+				printf("%f", va_arg(args, double)), separator_indicator = 1;
 				break;
 			case 's':
 				s_val = va_arg(args, char *);
@@ -44,11 +42,9 @@ void print_all(const char * const format, ...)
 					printf("(nil)");
 					break;
 				}
-				printf("%s", s_val);
+				printf("%s", s_val), separator_indicator = 1;
 				break;
-			default:
-				break;
-		} separator_indicator = 1;
+		}
 		i++;
 	} va_end(args), printf("\n");
 }
