cea45f159e10f77a89d211033a022fcf8f27545a
Hovixen <orevaerijo@gmail.com>
2023-08-19 13:00:47 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,44 +9,51 @@
  **/
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, separator_indicator = 0, n;
 	va_list args;/*initializing the list argument*/
-	int i_val, separator_indicator = 0;
+	int i_val /*separator_indicator = 0*/;
 	char c;
 	char *s_val;
+	const char format_str[] = "cifs";
 
 	va_start(args, format);
 	while (format && format[i])
 	{
 		c = format[i];
-		if (separator_indicator != 0)
-			printf(", ");
+		n = 0;
+		while (format_str[n])
+		{
+			if ((separator_indicator != 0) && c == format_str[n])
+			{
+				printf(", ");
+				break;
+			} n++;
+		}
 		switch (c)
 		{
 			case 'c':
 				i_val = va_arg(args, int); /*char being promoted to an int*/
 				printf("%c", i_val), separator_indicator = 1;
 				break;
-			case 'i':
-				/*i_val = va_arg(args, int);*/
+			case 'i':/*i_val = va_arg(args, int);*/
 				printf("%d", va_arg(args, int)), separator_indicator = 1;
 				break;
 			case 'f':
 				/*f_val = va_arg(args, double);*float being promoted to a double*/
-				printf("%f", va_arg(args, double)), separator_indicator = 1;
+				printf("%.6f", va_arg(args, double)), separator_indicator = 1;
 				break;
 			case 's':
-				s_val = va_arg(args, char *);
-				if (s_val == NULL)
+				s_val = va_arg(args, char *), separator_indicator = 1;
+				if (!s_val)
 				{
 					printf("(nil)");
 					break;
 				}
-				printf("%s", s_val), separator_indicator = 1;
+				printf("%s", s_val); /*separator_indicator = 1;*/
 				break;
 		}
-		if (c != 's' && !separator_indicator)
-			print (" ");
+		/*separator_indicator = 1;*/
 		i++;
-	} va_end(args), printf("\n");
+	} printf("\n"), va_end(args);
 }
+
