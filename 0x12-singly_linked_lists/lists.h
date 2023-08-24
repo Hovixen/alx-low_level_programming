eb5e515fe142f9417026f98029ca932a787c5c02
Hovixen <orevaerijo@gmail.com>
2023-08-24 18:12:13 +0000

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef LISTS_H
+#ifndef LISTS_H_
 #define LISTS_H_
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -11,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
