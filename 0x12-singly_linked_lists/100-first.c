a3410be08b9532d684d55eee69d7a5a3009f2c1f
Hovixen <orevaerijo@gmail.com>
2023-08-25 02:19:26 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * __attribute__((constructor)) print_before_main - function
+ * prints this code before the main function.
+ * Return: returns void
+ */
+
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You are beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
