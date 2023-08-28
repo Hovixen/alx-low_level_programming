df29aa742b5397a5fed556b8b91299c4ad0c251b
Hovixen <orevaerijo@gmail.com>
2023-08-28 15:41:06 +0000

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listinit_len - functions prints the number of element
+ * in a linked list
+ * @h: pointer to the new node created
+ * Return: returns the number of node in a list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
