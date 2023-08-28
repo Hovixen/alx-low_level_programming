966ff1b3fc1136362364bc1d9736e1d3adca816f
Hovixen <orevaerijo@gmail.com>
2023-08-28 18:46:56 +0000

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,9 +2,9 @@
 /**
  * *add_nodeint - function adds new node at the beginnig of a structure list
  * @head: pointer of the pointer of the head of the list
- * @n: integer parameter of the list 
+ * @n: integer parameter of the list
  * Return: returns the address of the new node or NULL when failed
- */ 
+ */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
