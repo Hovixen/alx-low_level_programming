68593998e487e500cba118b09a4125886a6079c5
Hovixen <orevaerijo@gmail.com>
2023-08-28 19:52:03 +0000

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,12 +10,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t inum = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		inum++;
+		nnodes++;
 	}
-	return (inum);
+	return (nnodes);
 }
