2172a59f6f39c4a039e55d44205fafda56b3be09
Hovixen <orevaerijo@gmail.com>
2023-08-28 18:10:16 +0000

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (0);
 	node->n = n;
 	node->next = *head;
-	*head = node->next;
+	*head = node
 	printf("%d\n", node->n);
 	free(node);
 	return (node);
