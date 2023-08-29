92042ce8a05ca8450695d23d7f0cb63580537ced
Hovixen <orevaerijo@gmail.com>
2023-08-29 15:51:45 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,5 +36,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->n = n;
 	newNode->next = subnode->next;
 	subnode->next = newNode;
-	return (subnode);
+	return (*head);
 }
