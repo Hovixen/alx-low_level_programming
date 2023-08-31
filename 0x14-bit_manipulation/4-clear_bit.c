ea6e1744dbeff0760aa71ed8437307993caf484b
Hovixen <orevaerijo@gmail.com>
2023-08-31 21:33:19 +0000

    task4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,6 +4,12 @@
  * clear_bit - function set the value of a bit to 0 at a given index
  * @n: pointer to the unsigned long integer given.
  * @index: index(position) given.
- * Return: return (1) success or (0) when failed.
+ * Return: return (1) success or (-1) when failed.
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned int) * 8))
+		return (-1);
+	*n = *n & ~(1UL << index);
+	return (1);
+}
