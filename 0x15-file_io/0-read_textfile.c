a2fc3e3bea13bd779f96ad7fbedf87831f9f10b4
Hovixen <orevaerijo@gmail.com>
2023-09-04 15:49:12 +0000

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - function reads a text file and prints it to
+ * the POSIX standard output.
+ * @filename: pointer to a constant character.
+ * @letters: size of the character to be printed.
+ * Return: returns the actuall number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;/*file descriptor*/
+	ssize_t read_num = 0, write_num = 0;
+	char instance[letters + 1];
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	read_num = read(file, instance, letters);
+	if (read_num == -1)
+	{
+		close(file);
+		return (0);
+	}
+	instance[read_num] = '\0';
+
+	write_num = write(STDOUT_FILENO, instance, read_num);
+	if (write_num == -1)
+		return (0);
+	close(file);
+	return (write_num);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+
+
+#endif
