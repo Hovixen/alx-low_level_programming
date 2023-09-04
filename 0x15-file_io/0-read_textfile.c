5e6142c709930ebb75a309152a4325757d61de49
Hovixen <orevaerijo@gmail.com>
2023-09-04 16:05:31 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;/*file descriptor*/
-	size_t read_num = 0, write_num = 0;
+	ssize_t read_num = 0, write_num = 0;
 	/*char instance[letters + 1];*/
 	char *instance;
 
