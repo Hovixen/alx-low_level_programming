edce4dd75ebf4a8a79a6e061fe0b2039f1ec39d5
Hovixen <orevaerijo@gmail.com>
2023-11-02 17:50:17 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,7 +23,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newTable->array = malloc(sizeof(hash_table_t) * size);
 	if (newTable->array == NULL)
 	{
-		free(newTable);
 		return (NULL);
 	}
 
