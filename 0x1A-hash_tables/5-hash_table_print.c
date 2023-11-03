956862788dc785923054547421e741681916fa61
Hovixen <orevaerijo@gmail.com>
2023-11-03 10:40:08 +0000

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *currNode;
+	int commaPrint = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		currNode = ht->array[i];
+		while (currNode)
+		{
+			if (commaPrint == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", currNode->key, currNode->value);
+			commaPrint = 1;
+			currNode = currNode->next;
+		}
+	}
+	printf("}\n");
+}
